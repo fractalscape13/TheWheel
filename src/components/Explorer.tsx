b5d0e8501830a59188a0a0fb2949bad9c902f5aa
Nathan Wangemann <51109257+nwangemann@users.noreply.github.com>
2024-10-02 17:00:31 -0600

    import

--- src/components/Explorer.tsx
+++ src/components/Explorer.tsx
@@ -5,6 +5,7 @@ import { showBones1965 } from "@services/1965-bones";
 import { showBones1966 } from "@services/1966-bones"; 
 import { showBones1967 } from "@services/1967-bones"; 
 import { showBones1968 } from "@services/1968-bones"; 
+import { showBones1969 } from "@services/1968-bones"; 
 import { showBones1970 } from "@services/1970-bones"; 
 import Touchable from "@components/Touchable";
 import { formatDate } from "@services/utils";
