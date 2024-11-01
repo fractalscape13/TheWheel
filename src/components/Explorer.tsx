c689218b5cc0159363c9dbcde24d1d855f97125c
Nathan Wangemann <51109257+nwangemann@users.noreply.github.com>
2024-11-01 16:09:01 -0600

    77

--- src/components/Explorer.tsx
+++ src/components/Explorer.tsx
@@ -15,24 +15,24 @@ import { showBones1974 } from "@services/1974-bones";
 import { showBones1975 } from "@services/1975-bones"; 
 import { showBones1976 } from "@services/1976-bones"; 
 import { showBones1977 } from "@services/1977-bones"; 
-import { showBones1978 } from "@services/1978-bones"; 
-import { showBones1979 } from "@services/1979-bones"; 
-import { showBones1980 } from "@services/1980-bones"; 
-import { showBones1981 } from "@services/1981-bones"; 
-import { showBones1982 } from "@services/1982-bones"; 
-import { showBones1983 } from "@services/1983-bones"; 
-import { showBones1984 } from "@services/1984-bones"; 
-import { showBones1985 } from "@services/1985-bones"; 
-import { showBones1986 } from "@services/1986-bones"; 
-import { showBones1987 } from "@services/1987-bones"; 
-import { showBones1988 } from "@services/1988-bones"; 
-import { showBones1989 } from "@services/1989-bones"; 
-import { showBones1990 } from "@services/1990-bones"; 
-import { showBones1991 } from "@services/1991-bones"; 
-import { showBones1992 } from "@services/1992-bones"; 
-import { showBones1993 } from "@services/1993-bones"; 
-import { showBones1994 } from "@services/1994-bones"; 
-import { showBones1995 } from "@services/1995-bones"; 
+// import { showBones1978 } from "@services/1978-bones"; 
+// import { showBones1979 } from "@services/1979-bones"; 
+// import { showBones1980 } from "@services/1980-bones"; 
+// import { showBones1981 } from "@services/1981-bones"; 
+// import { showBones1982 } from "@services/1982-bones"; 
+// import { showBones1983 } from "@services/1983-bones"; 
+// import { showBones1984 } from "@services/1984-bones"; 
+// import { showBones1985 } from "@services/1985-bones"; 
+// import { showBones1986 } from "@services/1986-bones"; 
+// import { showBones1987 } from "@services/1987-bones"; 
+// import { showBones1988 } from "@services/1988-bones"; 
+// import { showBones1989 } from "@services/1989-bones"; 
+// import { showBones1990 } from "@services/1990-bones"; 
+// import { showBones1991 } from "@services/1991-bones"; 
+// import { showBones1992 } from "@services/1992-bones"; 
+// import { showBones1993 } from "@services/1993-bones"; 
+// import { showBones1994 } from "@services/1994-bones"; 
+// import { showBones1995 } from "@services/1995-bones"; 
 
 import Touchable from "@components/Touchable";
 import { formatDate } from "@services/utils";
@@ -41,7 +41,7 @@ import * as SecureStore from "expo-secure-store";
 import { FAVORITE_SHOWS } from "../constants";
 
 type ExplorerProps = {
-  goToShow: (show: Show | null) => void;
+  goToShow: (show: Show | null, availableShowsOnSelectedDate:any) => void;
   setSelectedYear: (year: number | null) => void;
   selectedYear: number | null;
   isLoading: boolean;
@@ -73,7 +73,7 @@ const Explorer: React.FC<ExplorerProps> = ({
   const activeCollectionPreservedData = useMemo(() => {
     // these if conditions are the boundary between new data and backwards compatibility
     // the year date can be modified as new data is entered
-    if (selectedYear && (selectedYear > 1964 && selectedYear < 1977)) { 
+    if (selectedYear && (selectedYear > 1964 && selectedYear < 1978)) { 
       if(selectedYear === 1965) { return showBones1965 }
       if(selectedYear === 1966) { return showBones1966 }
       if(selectedYear === 1967) { return showBones1967 }
@@ -86,10 +86,11 @@ const Explorer: React.FC<ExplorerProps> = ({
       if(selectedYear === 1974) { return showBones1974 }
       if(selectedYear === 1975) { return showBones1975 }
       if(selectedYear === 1976) { return showBones1976 }
+      if(selectedYear === 1977) { return showBones1977 }
 
     }
 
-    if (selectedYear && (selectedYear >= 1976) && collectionSelection)  {
+    if (selectedYear && (selectedYear >= 1977) && collectionSelection)  {
       const key = `showCollection${selectedYear}`;
       const shows = collectionSelection[key] || [];
       return filterShows(shows);
@@ -101,7 +102,7 @@ const Explorer: React.FC<ExplorerProps> = ({
   const activeCollection = useMemo(() => {
     // these if conditions are the boundary between new data and backwards compatibility
     // the year date can be modified as new data is entered
-    if (selectedYear && (selectedYear > 1964 && selectedYear < 1977)) {
+    if (selectedYear && (selectedYear > 1964 && selectedYear < 1978)) {
 
       // identify which imported collection to access
       let collection = showBones1965;
@@ -119,10 +120,11 @@ const Explorer: React.FC<ExplorerProps> = ({
       if(selectedYear === 1974) { collection =  showBones1974 }
       if(selectedYear === 1975) { collection =  showBones1975 }
       if(selectedYear === 1976) { collection =  showBones1976 }
+      if(selectedYear === 1977) { collection =  showBones1977 }
 
 
       // create unique show date list
-      const getUniqueByDate = (array) => {
+      const getUniqueByDate = (array:[]) => {
         const seenDates = new Set();
         return array.filter(item => {
           const date = item.date;
@@ -137,7 +139,7 @@ const Explorer: React.FC<ExplorerProps> = ({
       const uniqueShowsByDate = getUniqueByDate(collection);
       return uniqueShowsByDate;
     }
-    if (selectedYear && (selectedYear >= 1976) && collectionSelection)  {
+    if (selectedYear && (selectedYear >= 1977) && collectionSelection)  {
       const key = `showCollection${selectedYear}`;
       const shows = collectionSelection[key] || [];
       return filterShows(shows);
