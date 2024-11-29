b537fb3127f6148c2955bbd811413aa1caf340a3
Joseph Wangemann <45412175+fractalscape13@users.noreply.github.com>
2024-11-28 20:26:33 -0800

    fix: add status bar

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { TamaguiProvider, Theme } from "tamagui";
 import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
 import Navigation from "./src/navigation";
 import config from "./tamagui.config";
 
@@ -9,6 +10,7 @@ const App = () => (
   <TamaguiProvider config={config} defaultTheme={"dark"}>
     <SafeAreaProvider>
       <Theme name={"dark"}>
+        <StatusBar style="light" />
         <NavigationContainer>
           <Navigation />
         </NavigationContainer>
